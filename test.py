901feeec87368a2d624e997cc16a0d67b8aef92c
Marcelo0307 <marcelop.silva1991@gmail.com>
2025-11-11 15:25:51 -0300

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+import pprint
+
+produtosDict = {
+    "Arroz":{
+        "value": 15.50
+
+},
+    "Feijão":{
+      "value": 8.90 
+    
+    },
+    "Macarrão": {
+        "value": 6.75
+    }
+}
+
+pp = pprint
+pp.print(produtosDict)
